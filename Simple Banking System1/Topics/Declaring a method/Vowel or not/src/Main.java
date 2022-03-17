86b0ab00de364da314e5238d893cfa615de3050c
Valentin Ibanescu <ibanescu.valentin@gmail.com>
2022-03-17 12:57:40 +0200

    method body return

---
+++ Simple Banking System1/Topics/Declaring a method/Vowel or not/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isVowel(char ch) {
+        return switch (Character.toLowerCase(ch)) {
+            case 'a', 'e', 'i', 'o', 'u' -> true;
+            default -> false;
+        };
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char letter = scanner.nextLine().charAt(0);
+        System.out.println(isVowel(letter) ? "YES" : "NO");
+    }
+}
